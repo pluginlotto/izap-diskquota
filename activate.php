c12004c2f9bcec052d4a7d08cddd75c39c0cbbae
Narinder kaur <narinder.kaur@izap.in>
2001-01-17 07:42:17 +0000

    removed forwading to settings

--- activate.php
+++ activate.php
@@ -1,18 +1,17 @@
 <?php
-/**************************************************
-* PluginLotto.com                                 *
-* Copyrights (c) 2005-2010. iZAP                  *
-* All rights reserved                             *
-***************************************************
-* @author iZAP Team "<support@izap.in>"
-* @link http://www.izap.in/
-* Under this agreement, No one has rights to sell this script further.
-* For more information. Contact "Tarun Jangra<tarun@izap.in>"
-* For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
-* Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
+
+/* * ************************************************
+ * PluginLotto.com                                 *
+ * Copyrights (c) 2005-2010. iZAP                  *
+ * All rights reserved                             *
+ * **************************************************
+ * @author iZAP Team "<support@izap.in>"
+ * @link http://www.izap.in/
+ * Under this agreement, No one has rights to sell this script further.
+ * For more information. Contact "Tarun Jangra<tarun@izap.in>"
+ * For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
+ * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
  */
 
 
-
-global $CONFIG;
-forward(IzapBase::setHref(array('context' =>'admin','action' =>'plugin_settings','vars' =>array(GLOBAL_IZAP_DISKQUOTA_PLUGIN),'page_owner' =>false)));
+IzapBase::pluginSetting(array('name' => 'izap_allowed_diskspace', 'plugin' => GLOBAL_IZAP_DISKQUOTA_PLUGIN, 'value' => 10));
