2a6ad48d063e307ecbf67c2eba6746175daec6e1
Puneet@izap <puneet.sethi@izap.in>
2012-04-01 04:57:29 +0530

    major upgrade in full functionality

--- start.php
+++ start.php
@@ -1,78 +1,107 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/* * ************************************************
+ * PluginLotto.com                                 *
+ * Copyrights (c) 2005-2010. iZAP                  *
+ * All rights reserved                             *
+ * **************************************************
+ * @author iZAP Team "<support@izap.in>"
+ * @link http://www.izap.in/
+ * @version {version} $Revision: {revision}
+ * Under this agreement, No one has rights to sell this script further.
+ * For more information. Contact "Tarun Jangra<tarun@izap.in>"
+ * For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
+ * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
  */
 
 
-define('GLOBAL_IZAP_DISKQUOTA_PLUGIN', 'izap-disk-quota');
+/**
+ *  defining global variables
+ */
+define('GLOBAL_IZAP_DISKQUOTA_PLUGIN', 'izap-diskquota');
 define('GLOBAL_IZAP_DISKQUOTA_PAGEHANDLER', 'diskquota');
 
 elgg_register_event_handler('init', 'system', 'func_disk_quota_init');
 
+/**
+ *
+ * integrates the plugin with izap_elgg_bridge is done here
+ */
 function func_disk_quota_init() {
-    global $CONFIG;
-    if (elgg_is_active_plugin(GLOBAL_IZAP_ELGG_BRIDGE)) {
+  global $CONFIG;
+  if (elgg_is_active_plugin(GLOBAL_IZAP_ELGG_BRIDGE)) {
     izap_plugin_init(GLOBAL_IZAP_DISKQUOTA_PLUGIN);
   } else {
     register_error('This plugin needs izap-elgg-bridge');
-    disable_plugin(GLOBAL_IZAP_CONTEST_PLUGIN);
+    disable_plugin(GLOBAL_IZAP_DISKQUOTA_PLUGIN);
   }
-    
-    elgg_register_page_handler(GLOBAL_IZAP_ANTISPAM_PAGEHANDLER, GLOBAL_IZAP_PAGEHANDLER);
 
-    elgg_register_event_handler('create', 'object', 'func_izap_diskquota_increment');
-    elgg_register_event_handler('create', 'group', 'func_izap_diskquota_increment');
-    elgg_register_event_handler('update', 'object', 'func_izap_diskquota_decrement');
-    elgg_register_event_handler('update', 'object', 'func_izap_diskquota_increment');
-    elgg_register_event_handler('update', 'group', 'func_izap_diskquota_increment');
-    elgg_register_event_handler('delete', 'object', 'func_izap_diskquota_decrement');
-    elgg_register_event_handler('delete', 'group', 'func_izap_diskquota_decrement');
-   
-   elgg_extend_view('icon/user/default', GLOBAL_IZAP_DISKQUOTA_PLUGIN.'/forms/user_settings');
-   elgg_extend_view('icon/user/default', GLOBAL_IZAP_DISKQUOTA_PLUGIN.'/user_status_profile');
-   elgg_extend_view('page/elements/sidebar',GLOBAL_IZAP_DISKQUOTA_PLUGIN.'/user_status_sidebar');
+  elgg_register_event_handler('create', 'object', 'func_izap_diskquota_increment');
+  elgg_register_event_handler('create', 'group', 'func_izap_diskquota_increment');
+  elgg_register_event_handler('update', 'object', 'func_izap_diskquota_decrement');
+  elgg_register_event_handler('update', 'object', 'func_izap_diskquota_increment');
+  elgg_register_event_handler('update', 'group', 'func_izap_diskquota_increment');
+  elgg_register_event_handler('delete', 'object', 'func_izap_diskquota_decrement');
+  elgg_register_event_handler('delete', 'group', 'func_izap_diskquota_decrement');
+
+  elgg_extend_view('icon/user/default', GLOBAL_IZAP_DISKQUOTA_PLUGIN . '/forms/user_settings');
+  elgg_extend_view('icon/user/default', GLOBAL_IZAP_DISKQUOTA_PLUGIN . '/user_status_profile');
+  if (!elgg_is_logged_in()) {
+    return '';
+  } else {
+    elgg_extend_view('page/elements/sidebar', GLOBAL_IZAP_DISKQUOTA_PLUGIN . '/user_status_sidebar');
+  }
 }
 
-//function func_diskquota_on_acivation() {
-//    return true;
-//    global $CONFIG;
-//    forward($CONFIG->wwwroot . '/pg/admin/plugin_settings/' . GLOBAL_IZAP_DISKQUOTA_PLUGIN);
-//}
-
-
+/**
+ * increment in the disk space allocation
+ * @param type $event
+ * @param type $object_type
+ * @param type $object
+ * @return type boolean
+ */
 function func_izap_diskquota_increment($event, $object_type, $object) {
-    // Final check for the right object
-    if (!method_exists($object, 'getOwnerEntity')) {
-        return TRUE;
-    }
 
-    // subtypes to skip
-    $array = array('plugin');
-    if (in_array($object->getSubtype(), $array)) {
-        return TRUE;
-    }
+  // Final check for the right object  
+  if (!method_exists($object, 'getOwnerEntity')) {
+    return True;
+  }
 
-    $izap_disk_quota = new IzapDiskQuota($object->getOwnerEntity());
-    $return = $izap_disk_quota->validate();
-    if (!$return) {
-        register_error(elgg_echo('izap-diskquota:limt_up'));
-    }
+  // subtypes to skip
+  $array = array('plugin');
+  if (in_array($object->getSubtype(), $array)) {
+    return True;
+  }
 
-    // save file size if any with this object
-    if ($return) {
-        $object->izap_diskspace_used = $izap_disk_quota->getCurrentUploadSize();
-    }
-    return $return;
+  // disk quota used space increased 
+  $izap_disk_quota = new IzapDiskQuota($object->getOwnerEntity());
+  $return = $izap_disk_quota->validate();
+  if (!$return) {
+    register_error(elgg_echo('izap-diskquota:limt_up'));
+  }
+
+  // save file size if any with this object
+  if ($return) {
+    $object->izap_diskspace_used = $izap_disk_quota->getCurrentUploadSize();
+  }
+  return $return;
 }
 
+/**
+ * decreament process in disk space allocation
+ * @param type $event
+ * @param type $object_type
+ * @param type $object
+ * @return type boolean
+ */
 function func_izap_diskquota_decrement($event, $object_type, $object) {
-    // Final check for the right object
-    if (!method_exists($object, 'getOwnerEntity')) {
-        return TRUE;
-    }
 
-    $izap_disk_quota = new IzapDiskQuota($object->getOwnerEntity());
-    $izap_disk_quota->releaseSpace($object);
+  // Final check for the right object
+  if (!method_exists($object, 'getOwnerEntity')) {
+    return True;
+  }
+
+  //disk qouta used space is released
+  $izap_disk_quota = new IzapDiskQuota($object->getOwnerEntity());
+  $izap_disk_quota->releaseSpace($object);
 }
--- views/default/izap-diskquota/css.php
+++ views/default/izap-diskquota/css.php
@@ -1,5 +1,4 @@
 <?php
-
 /* * ************************************************
  * PluginLotto.com                                 *
  * Copyrights (c) 2005-2010. iZAP                  *
@@ -7,16 +6,27 @@
  * **************************************************
  * @author iZAP Team "<support@izap.in>"
  * @link http://www.izap.in/
+ * @version {version} $Revision: {revision}
  * Under this agreement, No one has rights to sell this script further.
  * For more information. Contact "Tarun Jangra<tarun@izap.in>"
  * For discussion about corresponding plugins, visit http://www.pluginlotto.com/pg/forums/
  * Follow us on http://facebook.com/PluginLotto and http://twitter.com/PluginLotto
  */
+?>
+.izap_diskspace_user_status {
+font-size:0.8em;
+}
 
-//sets the default value of disk space on plugin activation
-IzapBase::pluginSetting(array(
-    'name' => 'izap_allowed_diskspace',
-    'plugin' => GLOBAL_IZAP_DISKQUOTA_PLUGIN,
-    'value' => 3196,
-    'override' => True
-));
+.izap_diskspace_user_status .outer_wrapper{
+-moz-border-radius:5px;
+-webkit-border-radius:5px;
+border-radius:5px;
+padding:2px;
+background-color:#fff;
+width: 98%;
+}
+
+.izap_diskspace_user_status  .outer_wrapper .inner_wrapper{
+background-color:#2F53B4;
+height: 5px;
+}
